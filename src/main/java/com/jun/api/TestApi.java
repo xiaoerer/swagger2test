69c977c7922b019edba98168207fa8b7a1b73b68
jun <527468075@qq.com>
2017-08-24 14:26:20 +0800

    修改 api_key验证机制

--- src/main/java/com/jun/api/TestApi.java
+++ src/main/java/com/jun/api/TestApi.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by zz on 2017/8/24.
  */
@@ -15,9 +17,16 @@ public class TestApi {
 
     @ApiOperation(value = "测试", notes = "")
     @RequestMapping(value = "/test",method = RequestMethod.GET)
-    public User getIndex(){
+    public User getIndex(HttpServletRequest request){
+
+        /*
+        这里是获取api_key用来进行权限验证的，从登陆接口返回一个token,然后拿着这个token就是一个api_key，
+        每一个请求都要带着这个token来验证，是否该用户已经登录
+         */
+        Object api_key=request.getHeader("api_key");
 
         System.out.println("测试成功");
+        System.out.println(api_key.toString());
         User user=new User();
         user.setId(1);
         user.setUsername("xiaoer");
--- target/swagger2test/WEB-INF/jsp/test/test.jsp
+++ target/swagger2test/WEB-INF/jsp/test/test.jsp
@@ -1,16 +1,13 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: zz
-  Date: 2017/8/24
-  Time: 11:23
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="utf-8" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
     <title>测试的页面标题</title>
 </head>
 <body>
     <h1>哈哈哈</h1>
+<form:form>
+
+</form:form>
 </body>
 </html>
